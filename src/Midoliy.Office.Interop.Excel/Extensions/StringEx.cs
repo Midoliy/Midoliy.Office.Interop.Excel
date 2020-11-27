e3ccd19b109168c24e95d7b4518e3b4a989f1921
Tatsuya Midorikawa <midorikawa@any-one.jp>
2020-11-27 11:10:31 +0900

    💡Update

--- src/Midoliy.Office.Interop.Excel/Extensions/StringEx.cs
+++ src/Midoliy.Office.Interop.Excel/Extensions/StringEx.cs
@@ -7,52 +7,25 @@ namespace Midoliy.Office.Interop
 {
     public static class StringEx
     {
-        static StringEx()
-        {
-            _columnNumberStorage = new Dictionary<string, int>();
-            _columnNameTable = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-        }
-
-        public static int ToColumnNumber(this string @this, int depth = 1_000)
+        public static int ToColumnNumber(this string @this)
         {
             if (!@this.IsValidColumnName())
                 throw new ArgumentException("The string must contain only A to Z.");
-            if (depth < 1)
-                throw new ArgumentException("'depth' must be greater than or equal to 1");
 
-            if (_columnNumberStorage.TryGetValue(@this, out int key))
-                return key;
-
-            var found = IntegerEx.FindKey(@this);
-            if (-1 < found)
-            {
-                found = found + 1;
-                _columnNumberStorage.Add(@this, found);
-                return found;
-            }
-
-            for (int i = 1; i < depth; i++)
-            {
-                if (@this == i.ToColumnName())
-                {
-                    _columnNumberStorage.Add(@this, i);
-                    return i;
-                }
-            }
-            return 0;
+            var index = 0u;
+            foreach (uint col in @this.ToUpper())
+                index = index * 26 + ((uint)col - (uint)'A' + 1);
+            return (int)index;
         }
 
         private static bool IsValidColumnName(this string @this)
         {
-            foreach (var c in @this)
+            foreach (uint c in @this.ToUpper())
             {
-                if (!_columnNameTable.Contains(c))
+                if (c < (uint)'A' && (uint)'Z' < c)
                     return false;
             }
             return true;
         }
-
-        private static readonly Dictionary<string, int> _columnNumberStorage;
-        private static readonly string _columnNameTable;
     }
 }
